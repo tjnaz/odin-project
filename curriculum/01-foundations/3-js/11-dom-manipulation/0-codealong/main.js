7b6a9086f1bc55bd220c1c10924c377ebcc9e568
Thaj Anaz <anaz.thaj@gmail.com>
2022-08-29 08:35:58 +0530

    Relocate the html and js files to the codealong directory

---
+++ curriculum/01-foundations/3-js/11-dom-manipulation/0-codealong/main.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('#container');
+
+// const content = document.createElement('div');
+// content.classList.add('content');
+// content.textContent = 'This is the glorious text-content!';
+
+// container.appendChild(content);
+
+const p = document.createElement('p');
+p.style.color = 'red';
+p.textContent = `Hey I'm red!`;
+container.appendChild(p);
+
+const h3 = document.createElement('h3');
+h3.style.color = 'blue';
+h3.textContent = `I'm a blue h3!`;
+container.appendChild(h3);
+
+
+const div = document.createElement('div');
+div.style.cssText = 'border: 2px solid black; background: pink';
+
+const h1 = document.createElement('h1');
+h1.textContent = `I'm in a div`;
+
+const p2 = document.createElement('p');
+p2.textContent = `ME TOO!`;
+
+div.appendChild(h1);
+div.appendChild(p2);
+
+container.appendChild(div);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function(e){
+    // console.log(e.target);
+    e.target.style.background = 'blue';
+})
