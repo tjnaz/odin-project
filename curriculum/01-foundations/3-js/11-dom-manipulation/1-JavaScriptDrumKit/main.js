2295d5664e78d56ca3cd6289b4fd60d55ac08223
Thaj Anaz <anaz.thaj@gmail.com>
2022-09-02 14:59:57 +0530

    Finish the drumkit exercise with me own tweaks

--- curriculum/01-foundations/3-js/11-dom-manipulation/1-JavaScriptDrumKit/main.js
+++ curriculum/01-foundations/3-js/11-dom-manipulation/1-JavaScriptDrumKit/main.js
@@ -1,7 +1,23 @@
-window.addEventListener('keydown', function (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+window.addEventListener('keydown', (e) => {
+    console.log(e)
+    // Capture the keypress with "key"
+    const audio = document.querySelector(`audio[key="${e.key}"]`)
+    const key = document.querySelector(`.key[key=${e.key}]`)
     if(!audio) return // stop the function from running altogether
-    audio.currentTime = 0
-    audio.play()
-    console.log(audio)
+    audio.currentTime = 0 // Make the audio play with rapid keypresses
+    audio.play() // Play the audio
+
+    // Add the class to keys
+    key.classList.add('playing')
 })
+
+function removeTransition(e) {
+    // console.log(e)
+    if (e.propertyName !== 'transform') return
+
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+// console.log(keys)
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
